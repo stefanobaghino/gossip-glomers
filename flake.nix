6626b8e633d7e2452c1ba85af06a678ca1abba6f
Stefano Baghino <stefano@baghino.me>
2023-06-29 15:39:57 +0200

    move maelstrom flake to https://github.com/stefanobaghino/maelstrom-nix

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = github:numtide/flake-utils;
-    maelstrom.url = "path:nix/maelstrom";
+    maelstrom-nix.url = "github:stefanobaghino/maelstrom-nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, maelstrom }:
+  outputs = { self, nixpkgs, flake-utils, maelstrom-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        maelstrom = maelstrom-nix.packages.${system}.default;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -18,7 +19,7 @@
             pkgs.nixpkgs-fmt
             pkgs.go
             pkgs.gotools
-            maelstrom.packages.${system}.default
+            maelstrom
           ];
         };
       }
